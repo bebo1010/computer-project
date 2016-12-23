a6e4e5eedb7d9f545fbefde96368d876fffb5c4f
張議隆 <nick900828@gmail.com>
2016-12-23 12:49:07 +0800

    Add files via upload

---
+++ 成績計算.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a; //a:ヘ计秖
+	cout << "块ヘ计秖" 
+	<< "1 瓣/璣/计//"
+	<< "2 瓣/璣/计///菌/そ"
+	<< "3 瓣/璣/计" 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+    }
